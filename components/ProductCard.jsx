65670b4581af592faaf39cdbcb57c32a5d569486
Phatpope <ikavo799@gmail.com>
2023-07-16 22:37:18 +0700

    3

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -2,43 +2,48 @@ import { getDiscountedPricePercentage } from "@/utils/helper";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-const ProductCard = ({ data: { attributes: p, id } }) => {
-    return (
-        <Link
-            href={`/product/${p.slug}`}
-            className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
-        >
-            <Image
-                width={500}
-                height={500}
-                src={p.thumbnail.data?.attributes.url}
-                alt={p.name}
-            />
-            <div className="p-4 text-black/[0.9]">
-                <h2 className="text-lg font-tektur">{p.name}</h2>
-                <div className="flex items-center text-black/[0.5]">
-                    <p className="mr-2 text-lg font-semibold">
-                        {p.price}&#8363;
-                    </p>
 
-                    {p.original_price && (
-                        <>
-                            <p className="text-base  font-tektur line-through">
-                                {p.original_price} &#8363;
-                            </p>
-                            <p className="ml-auto text-base font-tektur text-green-500">
-                                {getDiscountedPricePercentage(
-                                    p.original_price,
-                                    p.price
-                                )}
-                                % off
-                            </p>
-                        </>
-                    )}
-                </div>
-            </div>
-        </Link>
-    );
+const formatPrice = (price) => {
+  const formattedPrice = parseInt(price).toLocaleString("en-US");
+  return formattedPrice.replace(/,/g, ".");
+};
+
+const ProductCard = ({ data: { attributes: p, id } }) => {
+  const formattedPrice = formatPrice(p.price); // Format the price with separators for thousands
+
+  return (
+    <Link
+      href={`/product/${p.slug}`}
+      className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
+    >
+      <Image
+        width={500}
+        height={500}
+        src={p.thumbnail.data?.attributes.url}
+        alt={p.name}
+      />
+      <div className="p-4 text-black/[0.9]">
+        <h2 className="text-lg font-tektur">{p.name}</h2>
+        <div className="flex flex-col">
+          <p className="text-base font-tektur text-black">
+            {formattedPrice}₫
+          </p>
+
+          {p.original_price && (
+            <p className="text-sm font-tektur line-through text-gray-500">
+              {formatPrice(p.original_price)}₫
+            </p>
+          )}
+        </div>
+
+        {p.original_price && (
+          <p className="mt-2 text-sm font-tektur text-green-500">
+            {getDiscountedPricePercentage(p.original_price, p.price)}% off
+          </p>
+        )}
+      </div>
+    </Link>
+  );
 };
 
 export default ProductCard;
--- components/Search.jsx
+++ components/Search.jsx
@@ -39,22 +39,21 @@ const Search = ({ setSearchModal }) => {
   }, []);
 
   return (
-    <div className="search-modal fixed w-full h-full z-50 top-0 left-0 bg-white overflow-y-auto ml-20 mr-20">
-      <div className="absolute top-3 right-4 m-4 mr-45">
-        <MdClose
-          className="text-gray-500 hover:text-gray-700 cursor-pointer"
-          onClick={() => setSearchModal(false)}
-        />
-      </div>
-      <div className="p-4 mr-40">
+    <div className="search-modal fixed w-full h-full z-50 top-0 left-0 bg-white overflow-y-auto">
+      <div className="p-4 flex items-center justify-center">
         <input
           autoFocus
           type="text"
           placeholder="Search for products"
           value={query}
           onChange={onChange}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-gray-500"
+          className="w-full px-4 py-2 border-solid border-2 border-gray-600 rounded-md focus:outline-none focus:border-gray-500"
         />
+        <MdClose
+  className="text-red-500 hover:text-red-700 cursor-pointer text-2xl transition duration-300 ease-in-out transform hover:scale-110"
+  onClick={() => setSearchModal(false)}
+/>
+
       </div>
       <div className="p-4">
         {!data?.data?.length && (
@@ -62,29 +61,33 @@ const Search = ({ setSearchModal }) => {
             Start typing to see products you are looking for.
           </div>
         )}
-<div className="mt-4 flex flex-col items-center overflow-auto my-10">
+        <div className="mt-4 space-y-4">
           {data?.data?.map((item) => (
             <div
-              className="flex items-center border-b border-gray-300 py-2 hover:bg-gray-100 cursor-pointer"
+              className="flex items-center justify-center border-b border-gray-300 py-2 hover:bg-gray-100 cursor-pointer mx-auto w-2/3"
               key={item?.attributes?.slug}
               onClick={() => {
-                router.push("/product/[slug]", `/product/${item.attributes.slug}`);
+                router.push(
+                  "/product/[slug]",
+                  `/product/${item.attributes.slug}`
+                );
                 setSearchModal(false);
               }}
             >
-              <div className="flex-shrink-0">
+              <div className="flex-shrink-0 w-16 h-16 mr-4">
+                <img
+                  src={"" + item.attributes.thumbnail?.data?.attributes?.url}
+                  alt={item?.attributes?.name}
+                  className="object-contain w-full h-full"
+                />
+              </div>
+              <div className="flex flex-col">
                 <div className="text-sm font-tektur text-gray-900">
                   {item?.attributes?.subtitle}
                 </div>
                 <div className="text-sm text-gray-500">
                   {item?.attributes?.name}
                 </div>
-              </div>
-              <div className="w-32 h-32 object-contain">
-                <img
-                  src={"" + item.attributes.thumbnail?.data?.attributes?.url}
-                  alt={item?.attributes?.name}
-                />
               </div>
             </div>
           ))}
