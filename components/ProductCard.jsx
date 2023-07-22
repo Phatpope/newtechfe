93b4d94485de5d58a7e56b40a249c3d3c739ab60
Phatpope <108511649+Phatpope@users.noreply.github.com>
2023-07-22 11:02:53 +0700

    Update ProductCard.jsx

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -12,7 +12,7 @@ const ProductCard = ({ data: { attributes: p, id } }) => {
   const formattedPrice = formatPrice(p.price); // Format the price with separators for thousands
 
   return (
-<Link href={`/product/${p.slug}`}>
+<Link href={`/product/${p.slug}`}
 
       className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
     
