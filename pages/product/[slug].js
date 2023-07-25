4b22cf26e3e6d122d79045c360f5ce10fe485173
Phatpope <108511649+Phatpope@users.noreply.github.com>
2023-07-25 22:32:09 +0700

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -173,8 +173,7 @@ const ProductDetails = ({ product, products }) => {
           </div>
           {/* right column end */}
         </div>
-        <div className="text-[18px] font-semibold mb-2 leading-tight"></div>
-        <div className="bg-white flex justify-center items-center h-1/4  py-10">
+        <div className="bg-white flex justify-center items-center h-1/4  py-8">
           <Navigation  product={p}/>
         </div>
 
