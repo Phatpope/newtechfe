dc96708c7454907a07c816bf560401372c4ee473
Phatpope <108511649+Phatpope@users.noreply.github.com>
2023-08-21 15:48:34 +0700

    Update ImageCarousel.jsx

--- components/ImageCarousel.jsx
+++ components/ImageCarousel.jsx
@@ -25,7 +25,7 @@ const ImageCarousel = ({ images,text1,text2,text3,text4 }) => {
       <div
         class="ripple relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat shadow-lg dark:shadow-black/20"
         data-te-ripple-init data-te-ripple-color="light">
-        <img  src={images?.[0].attributes.url} class="w-full" alt={images?.[0]?.attributes.name} />
+        <img  src={images[0].attributes.url} class="w-full" alt={images[0]?.attributes.name} />
         <a href="#!">
           <div
             class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
@@ -42,7 +42,7 @@ const ImageCarousel = ({ images,text1,text2,text3,text4 }) => {
           <path stroke-linecap="round" stroke-linejoin="round"
             d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
         </svg>
-        {text1}
+        Travels
       </div>
       <p class="mb-6 text-sm text-neutral-500 dark:text-neutral-400">
         Published <u>14.01.2022</u> by
@@ -67,7 +67,7 @@ const ImageCarousel = ({ images,text1,text2,text3,text4 }) => {
       <div
         class="ripple relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat shadow-lg dark:shadow-black/20"
         data-te-ripple-init data-te-ripple-color="light">
-        <img  src={images?.[0].attributes.url} class="w-full" alt={images?.[0]?.attributes.name} />
+        <img  src={images[0].attributes.url} class="w-full" alt={images[0]?.attributes.name} />
         <a href="#!">
           <div
             class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
@@ -101,7 +101,7 @@ const ImageCarousel = ({ images,text1,text2,text3,text4 }) => {
       <div
         class="ripple relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat shadow-lg dark:shadow-black/20"
         data-te-ripple-init data-te-ripple-color="light">
-        <img  src={images?.[1].attributes.url} class="w-full" alt={images?.[0]?.attributes.name} />
+        <img  src={images[1].attributes.url} class="w-full" alt={images[0]?.attributes.name} />
         <a href="#!">
           <div
             class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
@@ -135,7 +135,7 @@ const ImageCarousel = ({ images,text1,text2,text3,text4 }) => {
     <div class="flex flex-wrap">
       <div class="mb-12 w-full shrink-0 grow-0 basis-auto lg:mb-0 lg:w-5/12">
         <div class="flex lg:py-12">
-          <img src={images?.[1].attributes.url}
+          <img src={images[1].attributes.url}
             class="z-[10] w-full rounded-lg shadow-lg dark:shadow-black/20 lg:ml-[50px]" alt="image" />
         </div>
       </div>
